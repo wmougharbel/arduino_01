bd2f9e7e8c61b6f5cdb05d2ea9d848855310f085
Walid Mougharbel <103928495+wmougharbel@users.noreply.github.com>
2023-11-13 18:05:42 +0100

    Update arduino01.ino
    fixed contrast issue

--- arduino01.ino
+++ arduino01.ino
@@ -1,10 +1,12 @@
 #include <LiquidCrystal.h>
 
 const int analogPin = A0;
+const int contrastPin = 8;
 const int ledPin = 10;
 const int rs = 11, en = 2, d4 = 3, d5 = 4, d6 = 5, d7 = 6;
 int brightness = 0;
 int sensor_val = 0;
+int constrastVal = 50
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup()
@@ -20,6 +22,7 @@ void loop()
   sensor_val = analogRead(analogPin);
   brightness = map(sensor_val, 0, 1023, 0, 255);
   analogWrite(ledPin, brightness);
+  analogWrite(contrastPin, contrastVal);
   lcd.print(sensor_val);
   lcd.print("    ");
 }
